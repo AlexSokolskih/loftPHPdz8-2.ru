f86b41e0f3732d0f5e7f39002438f53966954c1c
AlexSokolskih <xaam1@ya.ru>
2017-08-23 10:12:42 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sokolskih
+ * Date: 11.08.2017
+ * Time: 11:10
+ */
+
+include_once 'vkapi.php';
+
+
+
+//VkAPI::autorization();
+
+$vk = new VkAPI();
+$vk->user_id=374378008;
+
+$vk ->addImage();
+
+
+
+?>
+
+<html>
+    <head></head>
+    <body>
+    <form action="" method="post" type="multipart/data" >
+        <input type=file name=img>
+        <input type=submit name=img_load>
+    </form>
+    </body>
+</html>
---
+++ vkapi.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sokolskih
+ * Date: 11.08.2017
+ * Time: 13:13
+ */
+
+class VkAPI
+{
+   public $user_id='';
+   public $album_id='';
+   public $image_path='';
+   public static $config=array(
+       'client_id'=>'6155983',
+       'redirect_uri'=>'https://oauth.vk.com/blank.html',
+       'protectedKey'=>'5iAFDZE5WdOcBNUcTXOa',
+       'scope'=>'messages,wall');
+
+    public static $access=['messages','photos','friends','wall','offline'];
+
+    public $accessToken='fab0b9cb851b44969168c766a06b4b7afd99ab081e8f05734fdd574932d10fe36522bbd539fcfc47119b6';
+
+
+
+   public $code='c8942b02834d1cfa86';
+
+
+    public static function autorization()
+    {
+       // header('HTTP/1.1 301 Moved Permanently');
+       echo  'Location: https://oauth.vk.com/authorize?client_id='.VkAPI::$config["client_id"].'&redirect_uri='.VkAPI::$config["redirect_uri"].'&response_type=token&v=5.67&scope='.implode(',', VkAPI::$access);
+        // header('Location: https://oauth.vk.com/authorize?client_id='.VkAPI::$config["client_id"].'&redirect_uri='.VkAPI::$config["redirect_uri"].'&response_type=code&v=5.67&scope='.VkAPI::$config["scope"]);
+    }
+
+    public function getAccessToken(){
+        $ch = curl_init();
+
+// 2. указываем параметры, включая url
+        curl_setopt($ch, CURLOPT_URL, 'https://oauth.vk.com/access_token?client_id='.VkAPI::$config["client_id"].'&client_secret='.VkAPI::$config["protectedKey"].'&redirect_uri='.VkAPI::$config["redirect_uri"].'&code='.$this->code);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// 3. получаем HTML в качестве результата
+        $output = curl_exec($ch);
+        $output=json_decode($output);
+
+        echo '<br>    ';
+        var_dump($output);
+
+        $this->accessToken=$output->access_token;
+        echo '<br>';
+
+// 4. закрываем соединение
+        curl_close($ch);
+
+    }
+
+    public function addNotes(){
+        $ch = curl_init();
+
+// 2. указываем параметры, включая url
+        curl_setopt($ch, CURLOPT_URL, 'https://api.vk.com/method/messages.send?uid=374378008&message=ХабраХабр&title=Заголовок&access_token='.$this->accessToken);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+
+        $output = curl_exec($ch);
+        $output=json_decode($output);
+
+// 4. закрываем соединение
+        curl_close($ch);
+
+    }
+
+    public function addImage()
+    {
+       $server= json_decode(file_get_contents("https://api.vk.com/method/photos.getWallUploadServer?access_token=".$this->accessToken.'&v=5.67'));
+      var_dump($server->response);
+
+
+        $ch = curl_init();
+// 2. указываем параметры, включая url
+        curl_setopt($ch, CURLOPT_URL, $server->response->upload_url);
+
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+       // curl_setopt($ch, CURLOPT_HEADER, 0);
+
+
+        $post = array(
+            "photo"=>new CURLFile($_SERVER['DOCUMENT_ROOT']."/img/12.jpg")
+        );
+        var_dump($post);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+        $output = curl_exec($ch);
+        $output=json_decode($output);
+
+        echo '<br><br>';
+        var_dump($output);
+
+// 4. закрываем соединение
+        curl_close($ch);
+
+
+        $server= json_decode(file_get_contents("https://api.vk.com/method/photos.saveWallPhoto?access_token=".$this->accessToken.'&v=5.67&user_id='.$this->user_id.'&photo='.$output->photo.'&server='.$output->server.'&hash='.$output->hash));
+
+        echo '<br><br>';
+        echo "https://api.vk.com/method/photos.saveWallPhoto?access_token=".$this->accessToken.'&v=5.67&user_id='.$this->user_id.'&photo='.$output->photo.'&server='.$output->server.'&hash='.$output->hash;
+        var_dump($server);
+
+    }
+
+}
